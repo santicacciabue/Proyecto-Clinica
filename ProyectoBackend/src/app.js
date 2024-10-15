efa221dac5b2e885a06324a6be3b129e378aa722
Pablo Corujo <pablo.corujo@i2t-sa.com.ar>
2024-10-15 11:57:55 -0300

    agregado CORS

--- ProyectoBackend/src/app.js
+++ ProyectoBackend/src/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 //Routes
 import usuarioRoutes from "./routes/usuario.routes";
 import loginRoutes from "./routes/login.routes"
 
 const app = express();
+
+app.use(cors());
 
 //Settings
 app.set("port", 4000);
