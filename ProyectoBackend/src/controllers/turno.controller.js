914ef99c066a58f4eda73a27d0f3bf3434229cc0
Pablo Corujo <pablo.corujo@i2t-sa.com.ar>
2024-10-29 11:07:54 -0300

    agregado id_turno en respuesta obtenerTurnoMedico

--- ProyectoBackend/src/controllers/turno.controller.js
+++ ProyectoBackend/src/controllers/turno.controller.js
@@ -32,7 +32,7 @@ const obtenerTurnosMedico = async (req, res) => {
             fecha
         } = req.body
         const connection = await getConnection();
-        const response = await connection.query("SELECT CONCAT(u_paciente.apellido,', ', u_paciente.nombre) AS nombre_paciente,u_paciente.fecha_nacimiento, CONCAT(u_medico.apellido, ', ', u_medico.nombre) AS nombre_medico, t.fecha, t.hora, t.nota, c.nombre as cobertura FROM agenda a JOIN turno t ON a.id = t.id_agenda JOIN usuario u_paciente ON t.id_paciente = u_paciente.id AND u_paciente.rol = 'paciente' JOIN usuario u_medico ON a.id_medico = u_medico.id AND u_medico.rol = 'medico' JOIN cobertura c ON t.id_cobertura = c.id WHERE a.id_medico = ? AND t.fecha = ?", [id_medico, fecha]);
+        const response = await connection.query("SELECT CONCAT(u_paciente.apellido,', ', u_paciente.nombre) AS nombre_paciente,u_paciente.fecha_nacimiento, CONCAT(u_medico.apellido, ', ', u_medico.nombre) AS nombre_medico,t.id as id_turno, t.fecha, t.hora, t.nota, c.nombre as cobertura FROM agenda a JOIN turno t ON a.id = t.id_agenda JOIN usuario u_paciente ON t.id_paciente = u_paciente.id AND u_paciente.rol = 'paciente' JOIN usuario u_medico ON a.id_medico = u_medico.id AND u_medico.rol = 'medico' JOIN cobertura c ON t.id_cobertura = c.id WHERE a.id_medico = ? AND t.fecha = ?", [id_medico, fecha]);
         res.json({codigo: 200, mensaje: "OK", payload:  response});
     }
     catch(error){
